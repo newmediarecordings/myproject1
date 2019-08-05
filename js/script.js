e094ac880bd8dbc37830ab932af11a044912b6af
new media recordings <NewMediaRecordings@news-MacBook-Pro.local>
2019-08-05 17:03:56 -0500

    random colors in my squares

--- js/script.js
+++ js/script.js
@@ -1,9 +1,29 @@
 /*----- constants -----*/ 
+//cards array, all available cards
+const cards = [ 
+    {'color': 'red', 'hexcode': '#ff0000'}, 
+    {'color': 'orange', 'hexcode': '#ffa500'},
+    {'color': 'yellow', 'hexcode': '#ffff00'},
+    {'color': 'green', 'hexcode': '#008000'},
+    {'color': 'blue', 'hexcode': '#0000ff'},
+    {'color': 'purple', 'hexcode': '#800080'},
+    {'color': 'pink', 'hexcode': '#ffc0cb'},
+    {'color': 'teal', 'hexcode': '#008080'},
+];
+console.log(cards);
+
 
 /*----- app's state (variables) -----*/ 
 let winner;
+let score;
+let match;
+
 
 /*----- cached element references -----*/ 
+//computer needs to remember what was turned over
+//  ''       ''   ''  ''     what cards are still available
+// which cards match and keep score.
+
 let message = document.querySelector('.message');
 
 /*----- event listeners -----*/ 
@@ -11,12 +31,16 @@ document.querySelector('button').addEventListener('click', function() {
     console.log('clicked')
 });
 
-document.querySelector('.square1').addEventListener('click', function() {
-    console.log('clicked square1')
+document.querySelector('.square').addEventListener('click', function() {
+    console.log('clicked square') // i really want this to reveal a card. 
 });
 
 
+
 /*----- functions -----*/
+//shuffle cards
+//turn over cards
+
 
 function startTimer() {
     if(timer === 0) return;
@@ -25,15 +49,15 @@ function startTimer() {
     return startTimer();
 
 }
-init();
+// init();
 
 function init() {
     winner = null;
     timer = 60000;
     render();
 }
+//this is the timer
 let timer = 60000
-
 function updateTimer() {
     if(timer === 0) {
         console.log('Timer Finished');
@@ -50,3 +74,23 @@ function updateTimer() {
         return updateTimer();
     }, 1000)
 }
+//boilerplate random generator
+function getRandomInt(max) {
+    return Math.floor(Math.random() * Math.floor(max));
+  }
+//the for loop first starts at zero, then adds one eachtime it goes through the function
+//colorindex gets a random integer based on the card array's length
+//let square connects the html id to the cards index array
+  function fillColor() {
+      for(let i=0; i<8;i++) {
+          let colorIndex = getRandomInt(cards.length)
+          let square=document.getElementById(`square${i+1}`).style.backgroundColor=`${cards[colorIndex].color}`
+        
+    } 
+  }
+  //run this fillColor, is running that function there.
+fillColor()
+
+//next up! add event listeners to the cards!
+
+
